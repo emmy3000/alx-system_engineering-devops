8d66d58fa2268ad06321a79b92e765b2795742b9
emmy3000 <emodiemeka@gmail.com>
2023-05-03 19:36:15 -0700

    feat: Add Ruby script to match regular expression pattern

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,9 +1,5 @@
 #!/usr/bin/env ruby
 #script searches for the regex pattern 'hbt?n' in the input string and output its matches.
 #The regex matches 'hbtn' or 'hbtn' with 'b' optional.
-
-input_string=ARGV[0]
-pattern=/hb?n/
-
-puts input_string.scan(pattern).join(' ')
+puts ARGV[0].scan(/hb?tn/).join
 
