d0a0b70929ae37940381a80f9c1e24cc25769176
emmy3000 <emodiemeka@gmail.com>
2023-06-23 11:34:33 -0700

    Add Puppet manifest to install Flask 2.1.0 with pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-# Description: Puppet manifest to install Flask using pip3
+# Description: Puppet manifest to install Flask using pip3.
 
-# Define a package resource for flask with a specific version
+# Define a package resource for Flask with a specific version.
 package { 'flask':
-  ensure  => '2.1.0',
-  provide => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
