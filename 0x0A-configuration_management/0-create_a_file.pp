20087e29595ee078075728c08ae1e3df789fadea
emmy3000 <emodiemeka@gmail.com>
2023-06-23 11:25:11 -0700

    Add Puppet manifest for creating /tmp/school file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Define a file resource for /tmp/school with specific permission and content
 
-file { 'tmp/school':
-  ensure => file,
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'I love Puppet',
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
