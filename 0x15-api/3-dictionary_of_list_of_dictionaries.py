af016c87534bb5f070a93a0c1b1aafcaf5483802
Emeka Emodi <emodiemeka@gmail.com>
2023-07-24 23:01:31 -0700

    feat: Retrieve and export all employees' TODO progress in JSON

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,8 +3,8 @@
 Retrieve and display employees' TODO list progress using a REST API.
 Export data in JSON format.
 """
-import json
 import requests
+import json
 
 
 def get_all_employees_todo_progress():
@@ -40,6 +40,8 @@ def get_all_employees_todo_progress():
             ]
 
             all_data[str(employee_id)] = completed_tasks
+
+        print(json.dumps(all_data, indent=2))
 
         filename = "todo_all_employees.json"
         with open(filename, 'w') as json_file:
