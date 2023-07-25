964b13bd2fd380cf815ca2642e1e000058468722
Emeka Emodi <emodiemeka@gmail.com>
2023-07-24 23:13:35 -0700

    feat: Add script to display employee TODO progress from REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,14 +13,11 @@ if __name__ == '__main__':
                        .format(employee_id)).json()
     todo_data = rq.get(base_url + 'todos',
                        params={'userId': employee_id}).json()
-    completed_tasks = [title.get("title")
-                       for title in todo_data if title.get('completed')]
+    completed_tasks = [title.get("title") for title in todo_data
+                       if title.get('completed')]
 
-    print(completed_tasks)
-    print("Employee {} is done with tasks ({}/{})"
-          .format(user_data.get("name"),
-                  len(completed_tasks),
-                  len(todo_data)))
+    print("Employee Name: {}".format(user_data.get("name")))
+    print("To Do Count: {}/{}".format(len(completed_tasks), len(todo_data)))
 
     for title in completed_tasks:
         print("\t{}".format(title))
