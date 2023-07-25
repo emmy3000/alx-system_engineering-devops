73a69983162c0abded5a4f717f04a1f08d056f3c
Emeka Emodi <emodiemeka@gmail.com>
2023-07-24 22:14:12 -0700

    feat: Add script to display employee TODO progress from REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,52 +1,29 @@
 #!/usr/bin/python3
 """
-Retrieve and display an employee's TODO list progress using a REST API.
+Retrieve and display an employee's
+TODO list progress using a REST API.
 """
-import requests
+import requests as rq
+import sys
 
 
-def get_employee_todo_progress(employee_id):
-    """
-    Retrieve and display the employee's TODO list progress.
+def main():
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    employee_id = sys.argv[1]
 
-    Args:
-        employee_id (int): The ID of the employee.
+    user_data = rq.get(base_url + 'users/{}'.format(employee_id)).json()
+    todo_data = rq.get(base_url + 'todos',
+                       params={'userId': employee_id}).json()
+    completed_tasks = [title.get("title") for title in todo_data
+                       if title.get('completed')]
 
-    Returns:
-        None
-    """
-    base_url = "https://jsonplaceholder.typicode.com/"
-    employee_url = "{}users/{}".format(base_url, employee_id)
-    todos_url = "{}todos?userId={}".format(base_url, employee_id)
+    print(completed_tasks)
+    print("Employee {} is done with tasks ({}/{})"
+          .format(user_data.get("name"), len(completed_tasks), len(todo_data)))
 
-    try:
-        employee_res = requests.get(employee_url)
-        employee_data = employee_res.json()
-        employee_name = employee_data.get('name', 'Unknown Employee')
-
-        todos_res = requests.get(todos_url)
-        todos_data = todos_res.json()
-
-        completed_tasks = [task for task in todos_data
-                           if task.get('completed', False)]
-
-        progress_line = "Employee {} is done with tasks ({}/{})".format(
-            employee_name, len(completed_tasks), len(todos_data)
-        )
-        print(progress_line + ":")
-
-        for task in completed_tasks:
-            print("\t{}".format(task.get('title', 'N/A')))
-
-    except requests.exceptions.RequestException as e:
-        print("Error occurred while fetching data:", e)
+    for title in completed_tasks:
+        print("\t{}".format(title))
 
 
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py EMPLOYEE_ID")
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+if __name__ == '__main__':
+    main()
