d6c3badc412f45fce11734e040a0a71d4fb9bfac
Emeka Emodi <emodiemeka@gmail.com>
2023-07-24 23:09:39 -0700

    feat: Add script to display employee TODO progress from REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python3
 """
-Retrieve and display an employee's data using a REST API.
+Retrieve and display an employee's TODO list progress using a REST API.
 """
-import requests
+import requests as rq
 import sys
 
-
-def main():
+if __name__ == '__main__':
     base_url = 'https://jsonplaceholder.typicode.com/'
     employee_id = sys.argv[1]
 
-    user_data = requests.get(base_url + 'users/{}'
-                             .format(employee_id)).json()
-    todo_data = requests.get(base_url + 'todos',
-                             params={'userId': employee_id}).json()
-    completed_tasks = [title.get("title") for title in todo_data
-                       if title.get('completed')]
+    user_data = rq.get(base_url + 'users/{}'
+                       .format(employee_id)).json()
+    todo_data = rq.get(base_url + 'todos',
+                       params={'userId': employee_id}).json()
+    completed_tasks = [title.get("title")
+                       for title in todo_data if title.get('completed')]
 
-    employee_name = user_data.get("name")
-    total_tasks = len(todo_data)
-    completed_task_count = len(completed_tasks)
-
+    print(completed_tasks)
     print("Employee {} is done with tasks ({}/{})"
-          .format(employee_name, completed_task_count, total_tasks))
+          .format(user_data.get("name"),
+                  len(completed_tasks),
+                  len(todo_data)))
 
     for title in completed_tasks:
         print("\t{}".format(title))
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py EMPLOYEE_ID")
-    else:
-        main()
